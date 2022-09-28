5ff54093e5b1e8c8584e8724ba6b8d97f011209a
HMenezes <humberto.menezes47@hotmail.com>
2022-09-28 13:18:56 -0300

    new/featureV3

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
+for ( let contador = 0; contador < listaDeTeclas.length; contador ++){
 
-while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    const tecla =  listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];  
+    const idAudio = `#som_${instrumento}`;
+    // template string
 
-    contador = contador + 1;
+    tecla.onclick = function(){
+      tocaSom (idAudio)
+    }
 
-    console.log(contador);
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa')
+    }
 }
